99cb6b491cc39bb39898672d9dd1d6e9fa8f30e5
HYUNHO LEE <hyunho.lee862@gmail.com>
2019-06-05 17:52:15 +0900

    2nd commit

--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Board
 
 # Create your views here.
+def index(request):
+    boards = Board.objects.all()
+    context = {
+        'boards':boards,
+        }
+    return render(request, 'boards/index.html', context)
+
+
+def new(request):
+    return render(request, 'boards/new.html')
+
+
+def create(request):
+    # new 에서 넘어오는 제목과 내용을 저장
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    # orm title과 content에 위에서 넘어온 값을 할당
+    board = Board(title=title,
+                  content=content)
+    # db에 저장
+    board.save()
+
+    # create.html 페이지를 render
+    return redirect(f'/boards/{board.pk}')
+
+def detail(request, pk):
+    board = Board.objects.get(pk=pk)
+    context = {
+        'board':board,
+    }
+    return render(request, 'boards/detail.html', context)
--- crud/settings.py
+++ crud/settings.py
@@ -32,12 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'boards.apps.BoardsConfig',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
